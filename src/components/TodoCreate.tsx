92d7de41ece7995c32e47eed22a3d82703d82737
ihsanye <ihsany98@gmail.com>
2024-11-01 21:43:03 +0300

    createTodo

--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { createTodo } from '../redux/todoSlice'
+import { TodoType } from '../types/Types'
 
 function TodoCreate() {
+
+    const [newTodo, setNewTodo] = useState<string>('')
+
+    const dispatch = useDispatch()
+    const handleCreateTodo = () => {
+        if (newTodo.trim().length == 0) {
+            alert('Todo Giriniz')
+            return
+        }
+        const payload: TodoType = {
+            id: Math.floor(Math.random() * 9999),
+            content: newTodo
+        }
+        dispatch(createTodo(payload))
+        setNewTodo('')
+    }
+
     return (
         <div className='todo-create'>
-            <input className='todo-input' type="text" placeholder='Todo Giriniz...' />
-            <button className='todo-create-btn'>Olustur</button>
+            <input value={newTodo} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewTodo(e.target.value)} className='todo-input' type="text" placeholder='Todo Giriniz...' />
+            <button onClick={handleCreateTodo} className='todo-create-btn'>Olustur</button>
         </div>
     )
 }

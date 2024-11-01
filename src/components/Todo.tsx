f693856181b833041bacd21bab69e643b0d8b85b
ihsanye <ihsany98@gmail.com>
2024-11-02 01:55:18 +0300

    list and delete

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { FaCheck, FaRegEdit } from 'react-icons/fa'
 import { IoMdRemoveCircleOutline } from 'react-icons/io'
+import { TodoType } from '../types/Types'
+import { useDispatch } from 'react-redux'
+import { removeTodoById } from '../redux/todoSlice'
 
-function Todo() {
+interface TodoProps {
+    todoProps: TodoType
+}
+
+function Todo({ todoProps }: TodoProps) {
+    const { id, content } = todoProps
+    const dispatch = useDispatch()
+    const handleRemoveTodo = () => {
+        dispatch(removeTodoById(id))
+    }
+
     return (
         <div className='todo-block'>
-            <div>ben 1 todoyum</div>
+            <div>{content}</div>
             <div className='edit-btn'>
-                <IoMdRemoveCircleOutline className='edit-icon' />
+                <IoMdRemoveCircleOutline onClick={handleRemoveTodo} className='edit-icon' />
                 <FaRegEdit className='edit-icon' />
             </div>
         </div>

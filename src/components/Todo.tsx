570859ca3e9a1447d7e6efb6aaf3c24c4ba4797c
ihsanye <ihsany98@gmail.com>
2024-11-02 18:03:45 +0300

    update todo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaCheck, FaRegEdit } from 'react-icons/fa'
 import { IoMdRemoveCircleOutline } from 'react-icons/io'
 import { TodoType } from '../types/Types'
 import { useDispatch } from 'react-redux'
-import { removeTodoById } from '../redux/todoSlice'
+import { removeTodoById, updateTodoById } from '../redux/todoSlice'
 
 interface TodoProps {
     todoProps: TodoType
@@ -12,16 +12,31 @@ interface TodoProps {
 function Todo({ todoProps }: TodoProps) {
     const { id, content } = todoProps
     const dispatch = useDispatch()
+
+    const [editable, setEditable] = useState<boolean>(false)
+    const [newTodo, setNewTodo] = useState<string>(content)
+
     const handleRemoveTodo = () => {
         dispatch(removeTodoById(id))
     }
 
+    const handleUpdateTodo = () => {
+        const playload = {
+            id: id,
+            content: newTodo
+        }
+        dispatch(updateTodoById(playload))
+        setEditable(false)
+    }
+
     return (
         <div className='todo-block'>
-            <div>{content}</div>
+            {editable ? <input className='edit-input' type='text' value={newTodo} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewTodo(e.target.value)} /> : <div>{content}</div>}
             <div className='edit-btn'>
                 <IoMdRemoveCircleOutline onClick={handleRemoveTodo} className='edit-icon' />
-                <FaRegEdit className='edit-icon' />
+                {editable ? <FaCheck className='edit-icon' onClick={handleUpdateTodo} /> : <FaRegEdit className='edit-icon' onClick={() => setEditable(true)} />
+                }
+
             </div>
         </div>
     )
